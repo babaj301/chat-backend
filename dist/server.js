802717c1bf453b7dd8143df20f802ecfce48cfad
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-04-16 14:37:28 +0100

    rebuilding again

--- dist/server.js
+++ dist/server.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             "https://chat-frontend-ten-opal.vercel.app",
-            "http://localhost:3000/rooms",
+            "http://localhost:3000",
         ],
         methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
         credentials: true,
@@ -32,7 +32,7 @@ const ADMIN_PASSWORD = "123456";
 app.use(cors({
     origin: [
         "https://chat-frontend-ten-opal.vercel.app",
-        "http://localhost:3000/rooms",
+        "http://localhost:3000",
     ],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
